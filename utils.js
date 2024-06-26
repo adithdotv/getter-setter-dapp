ffd1eaccbc9420611313004e169fd8d7c216eddd
adithdotv <adithadiabd17@gmail.com>
2024-06-26 18:16:26 +0530

    commit

--- utils.js
+++ utils.js
@@ -2,4 +2,15 @@ const Web3 = require('web3')
 
 const web3 = new Web3('https://localhost:8545')
 
-console.log(web3)
+// console.log(web3)
+
+const privateKey = '0x7e4775d2521f36e6236c166bb1bc56767926af85b391656f64dc4cf8b8897d66'
+
+const myAccount = web3.eth.accounts.wallet.add(privateKey)
+
+// console.log(myAccount)
+
+module.exports ={
+    web3: web3,
+    myAccount: myAccount
+}
